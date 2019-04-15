9a6e37e9b31126a05d49186fc388951775b395fb
Cristian Leiva <cleiva94.cl@gmail.com>
2019-04-15 15:02:06 -0400

    signup and auth finished

--- src/App.js
+++ src/App.js
@@ -1,16 +1,93 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import logo from './logo.svg'
 import PokedexContainer from './containers/PokedexContainer'
-import './App.css';
+import Signup from './components/Signup'
+import Login from './components/Login'
+import {Route, Switch, Link, withRouter} from 'react-router-dom'
+import './App.css'
 
 class App extends Component {
-  render() {
+  state = {
+    user: {}
+  }
+
+  componentDidMount = () => {
+    if (localStorage.getItem('token')) {
+      fetch('http://localhost:3000/current_user', {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': localStorage.getItem('token')
+        }
+      })
+      .then( res => res.json())
+      .then(userJson => {
+        this.setState({
+          user: userJson.user
+        })
+      })
+    }
+  }
+
+  sendSignUp = (e, userObj) => {
+    e.preventDefault()
+    fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({user: userObj })
+    })
+    .then(res => res.json())
+    .then(userData => {
+      this.setState({
+        user: userData.user
+      }, () => {
+        localStorage.setItem("token", userData.jwt)
+        console.log('After signup', this.state.user)
+      })
+    })
+  }
+
+  handleLogin = (e, userObj) => {
+    e.preventDefault()
+    fetch('http://localhost:3000/login', {
+      method: 'POST',
+      headers: { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({user: userObj})
+    })
+    .then(res => res.json())
+    .then(userData => {
+      localStorage.setItem('token', userData.jwt)
+      this.setState({
+        user: userData.user
+      }, () => console.log('After Login', this.state.user))
+    })
+  }
+
+  handleLogOut = () => {
+    this.setState({
+      user: {}
+    })
+    localStorage.removeItem('token')
+    this.props.history.push('/login')
+  }
+
+  render () {
     return (
-      <div className="App">
-        <PokedexContainer/>
+      <div className='App'>
+      <Switch>
+        <Route path={'/signup'} render={() => <Signup sendSignUp={this.sendSignUp}/>}/>
+        <Route path={'/login'}  render={() => <Login handleLogin={this.handleLogin}/>}/>
+        <Route path={'/'} />
+      </Switch>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default withRouter(App)
